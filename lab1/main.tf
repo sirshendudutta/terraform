f36067d09da611f23a162ccc7c041f6e82e94b84
sirshendudutta <officesd88@gmail.com>
2024-07-05 12:12:32 -0600

    ec2 creation using terraform

---
+++ lab1/main.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+    region = "us-east-1"
+}
+
+resource "aws_instance" "terraform_instance" {
+  ami = var.ami_id
+  instance_type = var.instance_type
+  availability_zone = var.availability_zone
+  key_name = var.key_name
+  vpc_security_group_ids = ["var.security_group_ids"]
+  tags = {
+    Name = "Terraform_Instance1"
+  }
+}
---
+++ lab1/variable.tf
@@ -0,0 +1,24 @@
+variable "ami_id" {
+    default = "ami-06c68f701d8090592"
+  
+}
+
+variable "instance_type" {
+    default = "t2.micro"
+  
+}
+
+variable "availability_zone" {
+    default = "us-east-1a"
+  
+}
+
+variable "key_name" {
+    default = "mypockey"
+  
+}
+
+variable "security_group_ids" {
+    default = "sg-0c0817ac303de553a"
+  
+}
